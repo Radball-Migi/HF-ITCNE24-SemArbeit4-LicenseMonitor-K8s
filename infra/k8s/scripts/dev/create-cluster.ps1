6a410f3eeadf715964796597ca68025808fa6203
Miguel Schneider <miguel@schneiderlein.ch>
2026-01-28 01:54:37 +0100

    update demo envirement

--- infra/k8s/scripts/dev/create-cluster.ps1
+++ infra/k8s/scripts/dev/create-cluster.ps1
@@ -1,8 +1,8 @@
-minikube start -p "semar4-dev"
+minikube start -p "semar4-demo"
     kubectl create namespace argocd
     kubectl create namespace licensetool
 
-minikube addons enable ingress -p semar4-dev
+minikube addons enable ingress -p semar4-demo
 
 kubectl apply -f "C:\Users\miguel.schneider\OneDrive - TBZ\GitHub_Repos_HF\HF-ITCNE24-SemArbeit4-LicenseMonitor-K8s\infra\k8s\apps\ingress-nginx\base\ingress-nginx-controler-lb.yaml"
 kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller
